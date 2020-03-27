5775af1d0a35d83b476de577e81e80cb4d892996
JOSHUA KAO <joshuakao@JOSHUAs-MacBook-Pro.local>
2020-03-26 23:47:37 -0700

    build dev if closed

--- src/build_pr.py
+++ src/build_pr.py
@@ -12,15 +12,29 @@ def handler(event, context):
         sqs = boto3.client('sqs')
         response = sqs.send_message(
             QueueUrl=os.environ['BUILD_SQS_URL'],
-            MessageBody=gh_archive_url(body)
+            MessageBody=gh_feature_archive(body)
+        )
+        print(response)
+    
+    if action == "closed":
+        sqs = boto3.client('sqs')
+        response = sqs.send_message(
+            QueueUrl=os.environ['BUILD_SQS_URL'],
+            MessageBody=gh_dev_archive(body)
         )
         print(response)
 
     return {"statusCode": 200}
 
 
-def gh_archive_url(body):
+def gh_feature_archive(body):
     # builds url to branch archive .zip
     repo = body['pull_request']['head']['repo']['full_name']
     branch = body['pull_request']['head']['ref']
     return "https://github.com/%s/archive/%s.zip" % (repo, branch)
+
+
+def gh_dev_archive(body):
+    # builds url to branch archive .zip
+    repo = body['pull_request']['head']['repo']['full_name']
+    return "https://github.com/%s/archive/dev.zip" % repo
