1891039b04fa2918755b48d2a9fd767302e29404
JOSHUA KAO <joshuakao@JOSHUAs-MacBook-Pro.local>
2020-03-26 23:48:41 -0700

    notes

--- src/build_pr.py
+++ src/build_pr.py
@@ -28,13 +28,13 @@ def handler(event, context):
 
 
 def gh_feature_archive(body):
-    # builds url to branch archive .zip
+    # builds url to feature/* branch archive .zip
     repo = body['pull_request']['head']['repo']['full_name']
     branch = body['pull_request']['head']['ref']
     return "https://github.com/%s/archive/%s.zip" % (repo, branch)
 
 
 def gh_dev_archive(body):
-    # builds url to branch archive .zip
+    # builds url to dev branch archive .zip
     repo = body['pull_request']['head']['repo']['full_name']
     return "https://github.com/%s/archive/dev.zip" % repo
