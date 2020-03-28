d56235249845b19982490f126d86ec292d8b9f76
JOSHUA KAO <joshuakao@JOSHUAs-MacBook-Pro.local>
2020-03-27 18:09:23 -0700

    init

--- src/build_pr.py
+++ src/build_pr.py
@@ -1,40 +1,32 @@
 import json
 import boto3
 import os
-
+from gh_webhook_utils import GithubWebhookUtils as ghw
 
 def handler(event, context):
     body = json.loads(event['body'])
-    action = body['action']
     print(body)
 
-    if action in ['opened', 'synchronize']:
-        sqs = boto3.client('sqs')
-        response = sqs.send_message(
-            QueueUrl=os.environ['BUILD_SQS_URL'],
-            MessageBody=gh_feature_archive(body)
-        )
-        print(response)
-    
-    if action == "closed":
-        sqs = boto3.client('sqs')
-        response = sqs.send_message(
-            QueueUrl=os.environ['BUILD_SQS_URL'],
-            MessageBody=gh_dev_archive(body)
-        )
-        print(response)
+    action = body['action']
 
-    return {"statusCode": 200}
+    try:
+        if action in ['opened', 'synchronize']:
+            sqs = boto3.client('sqs')
+            response = sqs.send_message(
+                QueueUrl=os.environ['BUILD_SQS_URL'],
+                MessageBody=ghw.feature_archive_url(body)
+            )
+            print(response)
 
+        if action == "closed":
+            sqs = boto3.client('sqs')
+            response = sqs.send_message(
+                QueueUrl=os.environ['BUILD_SQS_URL'],
+                MessageBody=ghw.dev_archive_url(body)
+            )
+            print(response)
 
-def gh_feature_archive(body):
-    # builds url to feature/* branch archive .zip
-    repo = body['pull_request']['head']['repo']['full_name']
-    branch = body['pull_request']['head']['ref']
-    return "https://github.com/%s/archive/%s.zip" % (repo, branch)
+        return {"statusCode": 200}
 
-
-def gh_dev_archive(body):
-    # builds url to dev branch archive .zip
-    repo = body['pull_request']['head']['repo']['full_name']
-    return "https://github.com/%s/archive/dev.zip" % repo
+    except Exception as e:
+        print(e)
