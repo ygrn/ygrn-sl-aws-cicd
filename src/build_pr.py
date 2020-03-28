29f26bdb132aa3f69ef12de77d25548c396dbcd3
JOSHUA KAO <joshuakao@JOSHUAs-MacBook-Pro.local>
2020-03-27 18:48:03 -0700

    better

--- src/build_pr.py
+++ src/build_pr.py
@@ -8,38 +8,39 @@ def handler(event, context):
     print(body)
 
     action = body['action']
+    repo = body['pull_request']['head']['repo']['full_name']
+    branch = body['pull_request']['head']['ref']
+    print(action, repo, branch)
 
     try:
         if action in ['opened', 'synchronize']:
             sqs = boto3.client('sqs')
             response = sqs.send_message(
                 QueueUrl=os.environ['BUILD_SQS_URL'],
-                MessageBody=feature_archive_url(body)
+                MessageBody=feature_archive_url(repo, branch)
             )
             print(response)
-
+        
         if action == "closed":
             sqs = boto3.client('sqs')
             response = sqs.send_message(
                 QueueUrl=os.environ['BUILD_SQS_URL'],
-                MessageBody=dev_archive_url(body)
+                MessageBody=dev_archive_url(repo)
             )
             print(response)
 
         return {"statusCode": 200}
 
     except Exception as e:
-        print(e)
+        print("[ERROR]", e)
+        return {"statusCode": 500}
 
 
-def feature_archive_url(body):
+def feature_archive_url(repo, branch):
     # builds url to feature/* branch archive .zip
-    repo = body['pull_request']['head']['repo']['full_name']
-    branch = body['pull_request']['head']['ref']
     return "https://github.com/%s/archive/%s.zip" % (repo, branch)
 
-    
-def dev_archive_url(body):
+
+def dev_archive_url(repo):
     # builds url to dev branch archive .zip
-    repo = body['pull_request']['head']['repo']['full_name']
     return "https://github.com/%s/archive/dev.zip" % repo
