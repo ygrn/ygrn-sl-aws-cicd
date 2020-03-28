4bc812a926c3bdd3a3a0cec991bcea270dfd85b0
JOSHUA KAO <joshuakao@JOSHUAs-MacBook-Pro.local>
2020-03-27 18:43:20 -0700

    bah

--- src/build_pr.py
+++ src/build_pr.py
@@ -32,15 +32,14 @@ def handler(event, context):
         print(e)
 
 
-    def feature_archive_url(body):
-        # builds url to feature/* branch archive .zip
-        repo = body['pull_request']['head']['repo']['full_name']
-        branch = body['pull_request']['head']['ref']
-        return "https://github.com/%s/archive/%s.zip" % (repo, branch)
+def feature_archive_url(body):
+    # builds url to feature/* branch archive .zip
+    repo = body['pull_request']['head']['repo']['full_name']
+    branch = body['pull_request']['head']['ref']
+    return "https://github.com/%s/archive/%s.zip" % (repo, branch)
 
-
-    @staticmethod
-    def dev_archive_url(body):
-        # builds url to dev branch archive .zip
-        repo = body['pull_request']['head']['repo']['full_name']
-        return "https://github.com/%s/archive/dev.zip" % repo
+    
+def dev_archive_url(body):
+    # builds url to dev branch archive .zip
+    repo = body['pull_request']['head']['repo']['full_name']
+    return "https://github.com/%s/archive/dev.zip" % repo
